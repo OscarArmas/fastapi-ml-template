093a9c93e30818ca55aa94b67732e26efad81acc
OscarArmass <oscar.armas@clara.team>
2024-06-19 21:51:45 -0600

    add exception itemNotFound

--- application/main/domain/exceptions.py
+++ application/main/domain/exceptions.py
@@ -6,3 +6,10 @@ class InvalidPrice(Exception):
 class PriceIsLessThanOrEqualToZero(Exception):
     def __init__(self):
         super().__init__("The price is less than or equal to zero")
+
+
+class ItemNotFound(Exception):
+    def __init__(self, item_id: str):
+        self.message = f"item_id: {item_id} not found"
+        self.status_code = 404
+        super().__init__(self.message)
