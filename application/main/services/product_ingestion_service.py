b3466185dd3fc380779f6a7dd7873dfaeae71b05
OscarArmass <oscar.armas@clara.team>
2024-06-21 09:35:29 -0600

    fix typo

--- application/main/services/product_ingestion_service.py
+++ application/main/services/product_ingestion_service.py
@@ -7,5 +7,5 @@ class ProductIngestionService:
     def __init__(self, db: DataBase):
         self.db = db
 
-    async def ingest_batch(self, product_list: ProductStatsList) -> bool:
+    async def ingest_batch(self, product_list: ProductStatsList) -> int:
         return await self.db.insert_multiple_db_record(product_list.to_dict_list())
